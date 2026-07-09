80a7c7b3c88cdfe98dfab39d1ca6e5ebd94caaf5
Walter Neils <45408786+Walter-Neils@users.noreply.github.com>
2026-07-09 12:32:14 -0700

    chore: misc

--- flake.nix
+++ flake.nix
@@ -13,12 +13,10 @@
   outputs = { self, nixpkgs, flake-utils, rust-overlay, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        # Apply the rust-overlay to our standard pkgs
         overlays = [ (import rust-overlay) ];
         pkgs = import nixpkgs { inherit system overlays; };
       in {
         devShells = {
-          # Import the eBPF shell, passing our overlay-aware pkgs 
           rust-ebpf = import ./rust/ebpf.nix { inherit pkgs; };
           nodejs-standard = import ./nodejs/standard.nix { inherit pkgs; };
         };
