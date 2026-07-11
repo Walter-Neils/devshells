f04a963795ba98ad7bb52eb4424aaf2a5c786a2c
Walter Neils <45408786+Walter-Neils@users.noreply.github.com>
2026-07-10 19:50:17 -0700

    fix: rust gtk4

--- rust/gtk4.nix
+++ rust/gtk4.nix
@@ -10,4 +10,15 @@
     pkgs.adwaita-icon-theme
     pkgs.gsettings-desktop-schemas
   ];
+  env = [
+    {
+      name = "PKG_CONFIG_PATH";
+      value = "${pkgs.gtk4}/lib/pkgconfig:${pkgs.libadwaita}/lib/pkgconfig:${pkgs.glib.dev}/lib/pkgconfig";
+    }
+
+    {
+      name = "XDG_DATA_DIRS";
+      value = "$GSETTINGS_SCHEMAS_PATH:$XDG_DATA_DIRS";
+    }
+  ];
 }
